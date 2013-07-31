a25e480b4d72d73ac9bd746c481d8d45ebdff5f1
Delapouite <delapouite@gmail.com>
2013-07-31 13:41:06 +0200

    transformed dump and parse links into buttons

--- public/js/view.js
+++ public/js/view.js
@@ -10,3 +10,24 @@ document.body.addEventListener('click', function(evt) {
 		tr.style.display = display === 'table-row' ? 'none' : 'table-row';
 	}
 });
+
+function remoteAction(evt, action) {
+	if(!evt.target.classList.contains(action)) {
+		return;
+	}
+	evt.target.style.color = 'green';
+	evt.target.textContent = 'today';
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '/' + action + '?uriPath=' + evt.target.dataset.uriPath);
+	xhr.send(null);
+}
+
+// dump buttons
+document.body.addEventListener('click', function(evt) {
+	remoteAction(evt, 'dump');
+});
+
+// parse buttons
+document.body.addEventListener('click', function(evt) {
+	remoteAction(evt, 'parse');
+});
